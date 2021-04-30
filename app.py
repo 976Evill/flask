4b8f88110c762646d55fa7dc3eeaf28b52661b1b
976Evil <an.lebedev@mail.ru>
2021-04-30 20:39:29 +0300

    1

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import request
+from flask import g
+from time import time
+
+from werkzeug.exceptions import BadRequest
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return 'First Flask Page'
+
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+
+
+
+
+
